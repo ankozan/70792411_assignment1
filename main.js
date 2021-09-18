8eec6e8817f64dd40a6063e94dfa87cb1baa755e
ankozanFHSU <82410548+ankozan@users.noreply.github.com>
2021-09-18 11:19:26 -0700

    Create main.js
    week 4

---
+++ main.js
@@ -0,0 +1,15 @@
+let myNumber = 5;
+let myOtherNumber = 5;
+let myTotal = myNumber * myOtherNumber;
+let myName = "";
+
+
+let myCombo = (myNumber + myOtherNumber).toString();
+let myOtherCombo = myNumber + myOtherNumber;
+
+let myRemainder = myNumber % 5;
+
+let i  = 2;
+let int = 4;
+let myBoolean = false;
+
